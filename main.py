17e3d4da7afb0d436001d3362e903c578b9677c5
alturet <myalture@gmail.com>
2021-10-01 18:47:03 +0800

    每周正常次数、30分钟以内早退次数、0分钟以上早退次数统计

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime
+starttime=datetime.datetime.now()
 
 df = pd.read_excel('罗浮中学_每日统计_20210901-20210925.xlsx', sheet_name='每日统计')
 
@@ -25,13 +26,13 @@ df = df.fillna(value=-1)
 
 
 # ---------计数功能实现------------
-def iswork(array: list) -> bool:
+def is_work(array: list) -> bool:
     '''
 
     :param array: ['请假','请假']
     :return: False
     '''
-    if array[0] == array[1] == '请假':
+    if array[0] == array[1] == '请假' or '补卡审批通过' in array :
         return False
     return True
 
@@ -57,14 +58,14 @@ def enough_work_time(array: list) -> bool:
 
     if array[0] == -1 or array[1] == -1:
         return False
-    '工作时间按3小时（180）分钟计算'
-    if get_work_time(array[1], array[0]) >= 180:
+    # 工作时间按3小时（180）分钟计算,晚自习则根据最后打卡时间超过八点计算
+    if get_work_time(array[1], array[0]) >= 180 or array[1][0:3]>='20':
         return True
 
     return False
 
 
-def get_work_count(attendance: list, leave: list) -> list:
+def get_work_times(data: list, attendance: list, leave: list) -> list:
     '''
 
     :param userid:  023508212226311234
@@ -74,29 +75,64 @@ def get_work_count(attendance: list, leave: list) -> list:
     :param leave:['请假','请假','请假','正常','缺卡','缺卡']
     :return:
     '''
+
     countresult = []
     for i in range(len(attendance)):
         c = 0
-        for j in range(0, len(attendance[0]), 2):
-            if c < 2:
-                if iswork(leave[i][j:j + 2]) is not True:
-                    c = c + 1
-                elif enough_work_time(attendance[i][j:j + 2]):
-                    c = c + 1
+        late=0
+        severly=0
+        # 一天都是休息，则代表法定节假日,也算老师打卡，除周六外
+        if leave.count('休息') == 6 and data[i][-3:] != '星期六':
+            c = 2
 
-        countresult.append(c)
+        # 一日最多两次计数统计
+        else:
+            for j in range(0, len(attendance[0]), 2):
+                if c < 2:
+                    if attendance[i][j] != -1 and attendance[i][j+1] != -1:
+                        worktime=get_work_time(attendance[i][j],attendance[i][j+1])
+                    # 请假,补卡也算正常打卡
+                    if is_work(leave[i][j:j + 2]) is not True:
+                        c = c + 1
+                    elif enough_work_time(attendance[i][j:j + 2]):
+                        c = c + 1
+                    elif 180-worktime>=30:
+                        severly+=1
+                    elif 0<180-worktime<30:
+                        late+=1
+        r=[c,late,severly]
+        countresult.append(r)
     return countresult
 
 
 
+# season='冬季打卡'
+# if season=='冬季打卡':
+#     night='20:40'
+# else:
+#     night='21:00'
 
+name=df.loc[:, '姓名'].values.tolist()
 attendance = df.loc[:, ['上班1打卡时间', '下班1打卡时间', '上班2打卡时间', '下班2打卡时间', '上班3打卡时间', '下班3打卡时间']].values.tolist()
+attendancedate = df.loc[:, '日期'].values.tolist()
 leave = df.loc[:, ['上班1打卡结果', '下班1打卡结果', '上班2打卡结果', '下班2打卡结果', '上班3打卡结果', '下班3打卡结果']].values.tolist()
-
+userid = df.loc[:, 'UserId'].values.tolist()
+week = ('星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六')
+backschool = '星期日'
 # ---------每日次数统计,并添加到总数据------------
+# 获取样式[1，0，1]，三个值分别代表每日：合格考勤次数，迟到次数，严重迟到次数
+attendancetimes = get_work_times(attendancedate, attendance, leave)
 
-result = get_work_count(attendance, leave)
-df['每日打卡次数'] = result
+x=[]
+y=[]
+z=[]
+for i in range(len(attendancetimes)):
+    x.append(attendancetimes[i][0])
+    y.append(attendancetimes[i][1])
+    z.append(attendancetimes[i][2])
+df['打卡次数'] = x
+df['缺30分钟以内次数'] = y
+df['缺20分钟以上次数'] = z
 # print(result)
 print(df)
 
@@ -104,8 +140,7 @@ print(df)
 
 
 # ---------每周次数统计------------
-attendancedate = df.loc[:, ['日期']].values.tolist()
-print(attendancedate)
+
 print(df.columns)
 # 周日作为第一天，周日晚至周五晚，至少 4.5 天弹性坐班
 # 晚自习不需要满3小时算一次
@@ -114,3 +149,55 @@ print(df.columns)
 # 连续旷工超过 15 个工作日或者一年内累计旷工超过 30 个工作 日的，按规定解除聘用合同
 # 未签到签退又未事前履行请假报告手续的视为缺勤
 # 每周因法定节假日导致不足5天，休息日视为两次
+
+# summary汇总记录每位教师每周打卡次数
+summary = {}
+
+# 缓存单位教师每周打卡次数
+normal = [0] * 7
+late=[0] * 7
+severly=[0] * 7
+#
+start=0
+end=-1
+for i in range(len(userid)):
+    id = str(userid[i])
+    d = attendancedate[i][-3:]
+
+    if id not in summary:
+        summary[id] = {}
+
+    normal[week.index(d)] = attendancetimes[i][0]
+    late[week.index(d)] = attendancetimes[i][1]
+    severly[week.index(d)] = attendancetimes[i][2]
+    # 由于数据在下周公布，所以在周六，记录上周情况
+    if week.index(d) == 6:
+        t=str(attendancedate[start])+' -> '+str(attendancedate[end])
+        summary[id]['姓名']=name[i]
+
+        # 早退次数考虑教师存在打卡次数过多问题，正常次数少于9次，才存在早退，优先统计30分钟以内迟到
+        x=sum(normal)
+        # y=9-x if x<9 and sum(late)>=9-x and sum(late)!=0 else 0
+        if x < 9:
+            if sum(late) >= 9 - x :
+                y=9-x
+                z=0
+            else:
+                y=sum(late)
+                z=9-x-y
+
+        summary[id][t] = {'正常打卡次数':x,
+                          '30分钟以内早退次数':y,
+                          '30分钟以上早退次数':z}
+        normal = [0] * 7
+        start=i+1
+        end=i
+    else:
+        end+=1
+
+
+for i in summary.items():
+    print(i)
+
+endtime=datetime.datetime.now()
+print((endtime-starttime).seconds)
