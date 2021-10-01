dfe0459bee798acd97dd3ced169b9f538891a205
alturet <myalture@gmail.com>
2021-10-01 20:55:07 +0800

    文件写入excel

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import openpyxl
 import pandas as pd
 import datetime
-starttime=datetime.datetime.now()
 
-df = pd.read_excel('罗浮中学_每日统计_20210901-20210925.xlsx', sheet_name='每日统计')
+filename = '罗浮中学_每日统计_20210901-20210925.xlsx'
+
+# 文件导入从周日或者周一开始，否则第一周情况会出现问题
+df = pd.read_excel(filename, sheet_name='每日统计')
 
 # ---------数据处理------------
 # 重设标题索引，丢弃无用行
@@ -22,9 +25,6 @@ df.index = index
 df = df.fillna(value=-1)
 
 
-# print(df)
-
-
 # ---------计数功能实现------------
 def is_work(array: list) -> bool:
     '''
@@ -32,7 +32,7 @@ def is_work(array: list) -> bool:
     :param array: ['请假','请假']
     :return: False
     '''
-    if array[0] == array[1] == '请假' or '补卡审批通过' in array :
+    if array[0] == array[1] == '请假' or '补卡审批通过' in array:
         return False
     return True
 
@@ -59,7 +59,7 @@ def enough_work_time(array: list) -> bool:
     if array[0] == -1 or array[1] == -1:
         return False
     # 工作时间按3小时（180）分钟计算,晚自习则根据最后打卡时间超过八点计算
-    if get_work_time(array[1], array[0]) >= 180 or array[1][0:3]>='20':
+    if get_work_time(array[1], array[0]) >= 180 or array[1][0:3] >= '20':
         return True
 
     return False
@@ -68,8 +68,6 @@ def enough_work_time(array: list) -> bool:
 def get_work_times(data: list, attendance: list, leave: list) -> list:
     '''
 
-    :param userid:  023508212226311234
-    :param username: 张三
     :param data: 21-09-01 星期三
     :param attendance: [06:52,11:45,13:52,17:14,0,0]
     :param leave:['请假','请假','请假','正常','缺卡','缺卡']
@@ -79,8 +77,8 @@ def get_work_times(data: list, attendance: list, leave: list) -> list:
     countresult = []
     for i in range(len(attendance)):
         c = 0
-        late=0
-        severly=0
+        late = 0
+        severly = 0
         # 一天都是休息，则代表法定节假日,也算老师打卡，除周六外
         if leave.count('休息') == 6 and data[i][-3:] != '星期六':
             c = 2
@@ -89,21 +87,20 @@ def get_work_times(data: list, attendance: list, leave: list) -> list:
         else:
             for j in range(0, len(attendance[0]), 2):
                 if c < 2:
-                    if attendance[i][j] != -1 and attendance[i][j+1] != -1:
-                        worktime=get_work_time(attendance[i][j],attendance[i][j+1])
+                    if attendance[i][j] != -1 and attendance[i][j + 1] != -1:
+                        worktime = get_work_time(attendance[i][j], attendance[i][j + 1])
                     # 请假,补卡也算正常打卡
                     if is_work(leave[i][j:j + 2]) is not True:
                         c = c + 1
                     elif enough_work_time(attendance[i][j:j + 2]):
                         c = c + 1
-                    elif 180-worktime>=30:
-                        severly+=1
-                    elif 0<180-worktime<30:
-                        late+=1
-        r=[c,late,severly]
+                    elif 180 - worktime >= 30:
+                        severly += 1
+                    elif 0 < 180 - worktime < 30:
+                        late += 1
+        r = [c, late, severly]
         countresult.append(r)
     return countresult
-
 
 
 # season='冬季打卡'
@@ -112,7 +109,7 @@ def get_work_times(data: list, attendance: list, leave: list) -> list:
 # else:
 #     night='21:00'
 
-name=df.loc[:, '姓名'].values.tolist()
+name = df.loc[:, '姓名'].values.tolist()
 attendance = df.loc[:, ['上班1打卡时间', '下班1打卡时间', '上班2打卡时间', '下班2打卡时间', '上班3打卡时间', '下班3打卡时间']].values.tolist()
 attendancedate = df.loc[:, '日期'].values.tolist()
 leave = df.loc[:, ['上班1打卡结果', '下班1打卡结果', '上班2打卡结果', '下班2打卡结果', '上班3打卡结果', '下班3打卡结果']].values.tolist()
@@ -123,16 +120,16 @@ backschool = '星期日'
 # 获取样式[1，0，1]，三个值分别代表每日：合格考勤次数，迟到次数，严重迟到次数
 attendancetimes = get_work_times(attendancedate, attendance, leave)
 
-x=[]
-y=[]
-z=[]
+x = []
+y = []
+z = []
 for i in range(len(attendancetimes)):
     x.append(attendancetimes[i][0])
     y.append(attendancetimes[i][1])
     z.append(attendancetimes[i][2])
 df['打卡次数'] = x
 df['缺30分钟以内次数'] = y
-df['缺20分钟以上次数'] = z
+df['缺30分钟以上次数'] = z
 # print(result)
 print(df)
 
@@ -155,49 +152,73 @@ summary = {}
 
 # 缓存单位教师每周打卡次数
 normal = [0] * 7
-late=[0] * 7
-severly=[0] * 7
+late = [0] * 7
+severly = [0] * 7
 #
-start=0
-end=-1
+start = 0
+end = -1
 for i in range(len(userid)):
     id = str(userid[i])
     d = attendancedate[i][-3:]
 
     if id not in summary:
-        summary[id] = {}
+        # summary[id] = {}
+        summary[id] = []
 
     normal[week.index(d)] = attendancetimes[i][0]
     late[week.index(d)] = attendancetimes[i][1]
     severly[week.index(d)] = attendancetimes[i][2]
     # 由于数据在下周公布，所以在周六，记录上周情况
     if week.index(d) == 6:
-        t=str(attendancedate[start])+' -> '+str(attendancedate[end])
-        summary[id]['姓名']=name[i]
+        t = str(attendancedate[start]) + ' -> ' + str(attendancedate[end])
+        # summary[id]['姓名']=name[i]
 
         # 早退次数考虑教师存在打卡次数过多问题，正常次数少于9次，才存在早退，优先统计30分钟以内迟到
-        x=sum(normal)
-        # y=9-x if x<9 and sum(late)>=9-x and sum(late)!=0 else 0
+        x = sum(normal)
+        # 如果遇到表格从周三等情况开始，则默认周一、二、三打卡正常
         if x < 9:
-            if sum(late) >= 9 - x :
-                y=9-x
-                z=0
+            if sum(late) >= 9 - x:
+                y = 9 - x
+                z = 0
             else:
-                y=sum(late)
-                z=9-x-y
+                y = sum(late)
+                z = 9 - x - y
 
-        summary[id][t] = {'正常打卡次数':x,
-                          '30分钟以内早退次数':y,
-                          '30分钟以上早退次数':z}
+        temp = [name[i], t, x, y, z]
+        # summary[id][t] = {'正常打卡次数':x,
+        #                   '30分钟以内早退次数':y,
+        #                   '30分钟以上早退次数':z}
+        summary[id].append(temp)
         normal = [0] * 7
-        start=i+1
-        end=i
+        start = i + 1
+        end = i
     else:
-        end+=1
+        end += 1
 
 
-for i in summary.items():
-    print(i)
+# 字典转化成列表
+# for key,value in summary.items():
+#     for i in range(len(value)):
 
-endtime=datetime.datetime.now()
-print((endtime-starttime).seconds)
+# 写入excel文件
+def write_excel(path: str, summary: dict):
+    title = ['姓名', '日期', '正常打卡次数', '缺30分钟以内次数', '缺30分钟以上次数']
+
+    workbook = openpyxl.load_workbook(filename)
+    sheet = workbook.create_sheet('按周统计汇总')
+
+    for i in range(5):
+        sheet.cell(row=1, column=i + 1, value=title[i])
+
+    i = 1
+    for key, val in summary.items():
+        print(val)
+        for j in range(len(val)):
+            for k in range(len(val[j])):
+                sheet.cell(row=i + 1, column=k + 1, value=val[j][k])
+            i += 1
+    workbook.save(path)
+    print('数据成功输出')
+
+
+write_excel(filename, summary)
