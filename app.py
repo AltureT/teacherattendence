bbd4e3f909ae1bce3c662fc1971af6e26832632d
alturet <myalture@gmail.com>
2021-10-02 09:13:23 +0800

    细节修改

--- app.py
+++ app.py
@@ -28,39 +28,39 @@ class Application(tk.Frame):
         file_path = filedialog.askopenfilename(title=u'选择文件', initialdir=(os.path.expanduser('~')))
         print('打开文件：', file_path)
         if file_path is not None:
-            with open(file=file_path, mode='r+', encoding='utf-8') as file:
+            # with open(file=file_path, mode='r+', encoding='utf-8') as file:
 
-                try:
-                    data = User(file_path)
-                    self.text1.insert('insert', '准备统计中·····\n')
-                except:
-                    self.text1.insert('insert', '文件导入出错·····\n')
+            try:
+                data = User(file_path)
+                self.text1.insert('insert', '准备统计中·····\n')
+            except:
+                self.text1.insert('insert', '文件导入出错·····\n')
 
-                try:
-                    attendancetimes = data.get_work_times()
-                    self.text1.insert('insert', '正在统计每日打卡情况·····\n')
-                except:
-                    self.text1.insert('insert', '数据统计出错，请检查·····\n')
+            try:
+                attendancetimes = data.get_work_times()
+                self.text1.insert('insert', '正在统计每日打卡情况·····\n')
+            except:
+                self.text1.insert('insert', '数据统计出错，请检查·····\n')
 
-                try:
-                    data.every_times_count(attendancetimes)
-                    self.text1.insert('insert', '正在各类情况汇总·····\n')
-                except:
-                    self.text1.insert('insert', '数据汇总出错，请检查·····\n')
+            try:
+                data.every_times_count(attendancetimes)
+                self.text1.insert('insert', '正在各类情况汇总·····\n')
+            except:
+                self.text1.insert('insert', '数据汇总出错，请检查·····\n')
 
-                try:
-                    summary = data.create_times_list(attendancetimes)
-                    self.text1.insert('insert', '正在数据生成中·····\n')
-                except:
-                    self.text1.insert('insert', '数据生成出错，请检查·····\n')
+            try:
+                summary = data.create_times_list(attendancetimes)
+                self.text1.insert('insert', '正在数据生成中·····\n')
+            except:
+                self.text1.insert('insert', '数据生成出错，请检查·····\n')
 
-                try:
-                    data.write_excel(file_path, summary)
-                    self.text1.insert('insert', '分析完成，打开刚刚下载的 "罗浮中学_每日打卡" 文件·····\n')
-                except:
-                    self.text1.insert('insert', '文件生成出错，请检查·····\n')
+            try:
+                data.write_excel(file_path, summary)
+                self.text1.insert('insert', '分析完成，打开刚刚下载的 "罗浮中学_每日打卡" 文件·····\n')
+            except:
+                self.text1.insert('insert', '文件生成出错，请检查·····\n')
 
-                print('分析完成，请打开刚刚下载的 "罗浮中学_每日打卡" 文件')
+            print('分析完成，请打开刚刚下载的 "罗浮中学_每日打卡" 文件')
 
     def open_file(self):
         os.system('open ' + file_path)
